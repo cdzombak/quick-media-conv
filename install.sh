460414b075bd75b6300a91bd385de6590e434e64
Chris Dzombak <chris@dzombak.com>
2023-01-19 10:05:46 -0500

    Cleanup scripts renamed in 54d732525078f781517d50a76ae928eaa7da6b57

--- install.sh
+++ install.sh
@@ -19,3 +19,19 @@ for fname in "${SCRIPTS[@]}"; do
 	set -e
 	chmod +x "$HOME/opt/bin/$fname"
 done
+
+echo "Cleaning up scripts renamed in 54d73252..."
+
+declare -a OLDSCRIPTS=(
+	"flv-to-mp4"
+	"tomp4hq"
+	"tomp4robust"
+)
+
+for fname in "${OLDSCRIPTS[@]}"; do
+	if [ -f "$HOME/opt/bin/$fname" ]; then
+		set +e
+		rm -i "$HOME/opt/bin/$fname"
+		set -e
+	fi
+done
